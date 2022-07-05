1727fdb14f68b2131cc079562cac387920080bdd
BekimHasani <bekimhasanni@gmail.com>
2022-07-05 16:12:55 +0200

    Flower seed data added in db & flower create api removed

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -6,16 +6,6 @@ class FlowersController < ApplicationController
     render json: @flowers, status: :ok
   end
 
-  def create
-    @flower = Flower.new(flower_params)
-    if @flower.save
-      render json: @flower, status: :created
-    else
-      render json: { errors: @flower.errors.full_messages },
-             status: :unprocessable_entity
-    end
-  end
-
   private
   def flower_params
     params.permit(:name, :description, :image)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#Delete all flowers in db
+Flower.destroy_all
+
+# Since I couldn't find flower in Faker gem
+# Flower db table population via seed data
+Flower.create!(name: "Daisy", description: "spring flower")
+      .image.attach(io: File.open('app/assets/images/daisy-flower.jpeg'), filename: 'daisy-flower.jpeg')
+Flower.create!(name: "Flax", description: "winter flower")
+      .image.attach(io: File.open('app/assets/images/flax-flower.jpeg'), filename: 'flax-flower.jpeg')
+Flower.create!(name: "Impatiens Balsamina", description: "spring flower")
+      .image.attach(io: File.open('app/assets/images/Impatiens_balsamina.jpeg'), filename: 'Impatiens_balsamina.jpeg')
+Flower.create!(name: "Jasmine", description: "autumn flower")
+      .image.attach(io: File.open('app/assets/images/Jasmine_Flower.jpeg'), filename: 'Jasmine_Flower.jpeg')
+Flower.create!(name: "Lavender", description: "summer flower")
+      .image.attach(io: File.open('app/assets/images/Lavender.jpg'), filename: 'Lavender.jpg')
+Flower.create!(name: "Lotus", description: "spring flower")
+      .image.attach(io: File.open('app/assets/images/lotus.jpeg'), filename: 'lotus.jpeg')
+Flower.create!(name: "Magnolia", description: "winter flower")
+      .image.attach(io: File.open('app/assets/images/Magnolia.jpeg'), filename: 'Magnolia.jpeg')
+Flower.create!(name: "Redrose", description: "autumn flower")
+      .image.attach(io: File.open('app/assets/images/redrose.jpeg'), filename: 'redrose.jpeg')
+Flower.create!(name: "Sunflower", description: "summer flower")
+      .image.attach(io: File.open('app/assets/images/sunflower.jpeg'), filename: 'sunflower.jpeg')
+Flower.create!(name: "Tulip", description: "spring flower")
+      .image.attach(io: File.open('app/assets/images/tulip-flower.png'), filename: 'tulip-flower.png')
+
+
